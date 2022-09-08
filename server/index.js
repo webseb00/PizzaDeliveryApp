834d3c5bcc92f42122f90d6e56a660b5006e2d87
webseb00 <lordow91@gmail.com>
2022-09-08 11:57:57 +0200

    Prepare to deploy

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const app = express();
 const connectDB = require('./db/config');
@@ -15,6 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/pizza', require('./routes/pizzaRoutes'))
 app.use('/api/admin', require('./routes/adminRoutes'))
 app.use('/api/order', require('./routes/orderRoutes'))
+
+// serve frontend
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => res.send('Please set to production...'))
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
